0f5e1a7a1a5cf05e19ef8a96d086b3bb94a58250
Timo Seitz <timo.seitz96@outlook.com>
2024-02-18 23:05:54 -0800

    fix for noNewEnvar => wait 5 min retry + pingProgress only while Running

--- AutoOptRunner.py
+++ AutoOptRunner.py
@@ -1,4 +1,4 @@
-import threading, os, csv
+import threading, os, csv, time
 from tokenize import String
 import requests
 import json
@@ -10,15 +10,23 @@ output_filepath = "output.txt"
 config = ""
 
 class Client:
+
+    status = ""
         
     def get_Variables():
         try:
             server_ip = f"http://{config['server_ip']}:5000"
             response = requests.get(f'{server_ip}/getEnVar')#
-            print(json.loads(response.text))
+
+            if response.status_code == 204:
+                print("No new EnVar, waiting for 5min")
+                time.sleep(300)
+                return Client.get_Variables()
+            print(f"EnVar: {json.loads(response.text)}")
             return json.loads(response.text)
         except Exception as e:
             print(e)
+        
 
     def create_Config(enVar):
         try:
@@ -35,13 +43,15 @@ class Client:
             raise Exception     
         
 
-    def run_Sim():
+    def run_Sim(self):
         try:
+            self.status = "RunSim"
             command = f'(cd && cd src/simopticon-main/build/ && ./simopticon ../config/simopticon.json)'
             result = subprocess.run(command, stdout=subprocess.PIPE, text=True, shell=True)
             with open(output_filepath,'w') as file:
                 file.write(result.stdout)
             tp = TextParser()
+            self.status = ""
             return tp.parse_data(output_filepath)
         except FileNotFoundError:
             print("FileError")
@@ -59,10 +69,11 @@ class Client:
             print(e)
 
 
-def progress():
+def progress(client):
     # Do this Task every 60s to inform the Server about progress
-    threading.Timer(60, progress).start()
+    threading.Timer(60, progress, client).start()
 
+    
     # Method to read last line from a file
     def read_last_line(file_path):
         with open(file_path, 'r', newline='') as file:
@@ -72,30 +83,35 @@ def progress():
                 return lines[-1]
             else:
                 return None
-
-    # Get Progress from File, spit csv to values and generate json
-    last_line = read_last_line(config['progresscsv_path'])[0]
-    values = str(last_line).split(";")
-    _progress = {
-        'Iteration':    values[0],
-        'Evaluation':   values[1],
-        'Minimum':      values[2]
-    }
-
-    # Send Progress Data to Server
-    server_ip = f"http://{config['server_ip']}:5000"
-    response = requests.post(f'{server_ip}/progress',json=_progress)
-    print(f"Pogress: {_progress}, Response: {json.loads(response.text)}")
+            
+    def pong():
+        try:
+            server_ip = f"http://{config['server_ip']}:5000"
+            response = requests.get(f'{server_ip}/ping')
+            print(json.loads(response.text))
+        except Exception as e:
+            print(e)
     
-def pong():
-    print("pong")
-    threading.Timer(30, pong).start()
-    try:
+    def pingprogress(data):
+        # Send Progress Data to Server
         server_ip = f"http://{config['server_ip']}:5000"
-        response = requests.get(f'{server_ip}/ping')
-        print(json.loads(response.text))
-    except Exception as e:
-        print(e)
+        response = requests.post(f'{server_ip}/progress',json=data)
+        print(f"Pogress: {data}, Response: {json.loads(response.text)}")
+
+    if client.status == "RunSim":
+        # Get Progress from File, spit csv to values and generate json
+        last_line = read_last_line(config['progresscsv_path'])[0]
+        values = str(last_line).split(";")
+        _progress = {
+            'Iteration':    values[0],
+            'Evaluation':   values[1],
+            'Minimum':      values[2]
+        }
+        pingprogress(_progress)       
+    else:
+        pong()
+    
+
 
 
 if __name__ == '__main__':
@@ -105,7 +121,9 @@ if __name__ == '__main__':
     with open(default_config_path, 'r') as config_file:
         config = json.load(config_file)  
 
-    progress()
+    client = Client()
+
+    progress(client)
 
     input("Press Enter to continue...")
 
@@ -115,7 +133,7 @@ if __name__ == '__main__':
         # create Config
         Client.create_Config(enVar)
         # run Sim
-        data = Client.run_Sim()
+        data = client.run_Sim()
         print(data)
         # report Data
         Client.report_Data(data, enVar)
